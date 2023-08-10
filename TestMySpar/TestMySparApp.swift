4eee5783622be92e089bf6561362d3ea7311e9f1
rampafan05 <110806636+rampafan05@users.noreply.github.com>
2023-08-10 13:48:50 +0300

    Initial Commit

---
+++ TestMySpar/TestMySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMySparApp.swift
+//  TestMySpar
+//
+//  Created by macbook on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestMySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
